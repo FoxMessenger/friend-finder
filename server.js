7bb1c15669d3e33fb5e3cce190b5a95ed25e727d
foxmessenger <iamliamfox@gmail.com>
2017-04-25 22:19:25 -0500

    working on app.use static to work properly

--- server.js
+++ server.js
@@ -1,16 +1,13 @@
 //======================
 //	DEPENDENCIES
 //======================
-var express = require('express');
-var bodyParser = require('body-parser');
 
-var app = express();
-// var path = require('path');
-
-
-var apiRoutes = require('./app/routing/apiRoutes')(app);
-
-var PORT = process.env.PORT || 3000;
+var bodyParser 	= require('body-parser');
+var express 	= require('express');
+var app 		= express();
+var path 		= require('path');
+var apiRoutes 	= require('./app/routing/apiRoutes')(app);
+var PORT 		= process.env.PORT || 3000;
 
 // this is used to parse the JSON that comes in
 // used to access the body from the route callback/return
@@ -20,12 +17,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+
 require('./app/routing/apiRoutes')(app);
 require('./app/routing/htmlRoutes')(app);
 
+
+app.use('/assets', express.static(__dirname + '/public'));
+// app.use(express.static('/public'));
+// app.use(express.static('files'));
+// app.use('/static', express.static(path.join(__dirname, '/public')));
+// app.use(express.static(path.join(__dirname, 'public')));
+
+// app.use('/assets',express.static(path.join(__dirname, '/public/assets')));
+// app.use('/js',express.static(path.join(__dirname, 'public/javascripts')));
+// app.use('/css',express.static(path.join(__dirname, 'public/stylesheets')));
+
 app.listen(PORT, function() {
-  console.log("App listening on PORT " + PORT);
+	console.log('App listening on PORT ' + PORT);
 });
+
+
+
+// this is to get the CSS files
 
 
 
